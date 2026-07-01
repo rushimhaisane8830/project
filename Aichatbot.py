b8d79695d33cd837438f3ff102501d843fe76310
rushimhaisane8830 <rushikeshmhaisane123@gmail.com>
2026-07-01 13:29:16 +0530

    Add files via upload

---
+++ Aichatbot.py
@@ -0,0 +1,65 @@
+import streamlit as st
+from langchain_huggingface import HuggingFaceEndpoint, ChatHuggingFace
+from langchain_core.messages import HumanMessage, AIMessage
+import os
+    
+    
+    
+token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+# Load model only once
+@st.cache_resource
+def load_model():
+
+    llm = HuggingFaceEndpoint(
+        repo_id="meta-llama/Llama-3.1-8B-Instruct",
+        huggingfacehub_api_token=token,
+        temperature=0.2,
+        max_new_tokens=500
+    )
+
+    return ChatHuggingFace(llm=llm)
+
+model = load_model()
+
+st.title("***********  AI Chatbot ************          ")
+
+# Chat history
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+
+# Display previous messages
+for msg in st.session_state.chat_history:
+
+    if isinstance(msg, HumanMessage):
+        with st.chat_message("user"):
+            st.write(msg.content)
+
+    elif isinstance(msg, AIMessage):
+        with st.chat_message("assistant"):
+            st.write(msg.content)
+
+# User input
+question = st.chat_input("Ask a question...")
+
+if question:
+
+    # Show user message
+    with st.chat_message("user"):
+        st.write(question)
+
+    st.session_state.chat_history.append(
+        HumanMessage(content=question)
+    )
+
+    # Get response
+    result = model.invoke(
+        st.session_state.chat_history
+    )
+
+    # Show bot response
+    with st.chat_message("assistant"):
+        st.write(result.content)
+
+    st.session_state.chat_history.append(
+        AIMessage(content=result.content)
+    )
